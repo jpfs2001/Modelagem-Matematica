812d6a0ee4919e02967df125ee1e3820cced2203
juan-nog <juanaraujo10@yahoo.com.br>
2021-05-20 04:21:24 -0300

    ajustes diversos e add grpah1

--- Projeto-2/algoritmo.py
+++ Projeto-2/algoritmo.py
@@ -12,7 +12,7 @@ k = 1 # Constante
 
 def main():
     # Legenda do Menu
-    print("Menu\n0 - Sair\n1 - Calcular o brilho em função de uma distancia em km pertencente ao intervalo [4.14 * 10^7 , 25.76 * 10^7] que o usuário digita \n2 - Descobrir qual é a distância r quando brilho é máximo e retorna um gráfico do brilho em função da distância r\n3 - Calcula p quando brilho é máximo\n4 - Calcula o brilho em função de um θ que o usuário digita\n5 - Descobrir qual o angulo θ quando brilho for máximo e retorna um gráfico do brilho em função do ângulo θ\n")
+    print("Menu\n0 - Sair\n1 - Calcular o brilho em função da distância r entre a Terra e Vênus. \n2 - Descobrir qual é a distância r quando o brilho é máximo e abrir o gráfico do brilho em função de r.\n3 - Calcular o brilho em função do ângulo θ entre Terra, Sol e Vênus. \n4 - Descobrir qual o ângulo θ quando brilho for máximo e abrir o gráfico do brilho em função de θ. \n5 - Calcular p quando brilho é máximo. \n")
 
     # Aqui sera usado um lupe infinito para que o usuário possa usar o código diversas vezes sem precisar execuar novamente
     while True: 
@@ -20,25 +20,26 @@ def main():
         if menu == 1:
             # Tratamento de dados para que o usuário só possa digitar um valor no intervalo
             while True:
-                # A distância será multiplicada por 10^7 para facilitar o usuário a digitar no intervalo desejado
-                r = float(input("Digite uma distância em km (ela vai ser multiplicada por 10^7): "))
-                r = r * (10**7)
+                # A distância será multiplicada por 10^6 para facilitar o usuário a digitar no intervalo desejado
+                r = float(input("Digite uma distância em milhões de km (entre 41.4 e 257.6): "))
+                r = r * (10**6)
                 if(r >= (D-d) and r <= (D+d)):
                         break
                 print("Valores fora do intervalo, tente outro valor!")
             
-            print("O brilho sera: ", (b(r) / maxb()) * 100, "% do bilho máximo")
+            print("O brilho será: ", (b(r) / maxb()) * 100, "% do bilho máximo.")
 
         elif menu == 2:
-            print("O brilho sera máximo na distancia r =", maxr()); 
+            print("O brilho será máximo na distância r =", maxr()/ 10**6, "milhões de km.")
+            graph1() 
         elif menu == 3:
-            print("O briho será máximo quando p =", pmax())
-        elif menu == 4:
             theta = float(input("Digite o valor de θ em graus: "))
-            print("O brilho será: ", (btheta(theta) / maxb()) * 100, "% do brilho máximo")
-        elif menu == 5:
-            print("O brilho será máximo quando θ =", maxtheta(),"Radianos")
+            print("O brilho será: ", (btheta(theta) / maxb()) * 100, "% do brilho máximo.")
+        elif menu == 4:
+            print("O brilho será máximo quando θ =", maxtheta(),"º.")
             graph2()
+        elif menu == 5:
+            print("O briho será máximo quando p =", pmax())
         elif menu == 0:
             break
         else:
@@ -69,29 +70,49 @@ def pmax():
 # função que encontra o theta quando brilho é máximo
 def maxtheta():
     r = maxr() # Pegando o valor máxido de r para usar na fórmula
-    theta  = math.acos((d**2 + D**2 - r**2) / (2*d*D))
+    theta  = 180/math.pi * math.acos((d**2 + D**2 - r**2) / (2*d*D))
     return theta
 
+# Função que retorna o gráfico θ x Brilho
 def graph2():
     # parâmetro r vai receber 5000 valores entre D-d até D+d. Representa a distância da Terra a Vênus.
     r = np.linspace((D-d), (D+d), 5000)
 
     # as funções paramétricas
-    theta = np.arccos((D**2 + d**2 - r**2)/(2*d*D))
+    theta = 180/math.pi * np.arccos((D**2 + d**2 - r**2)/(2*d*D))
     B = ((10**17)*(r**2 + d**2 + 2*d*r - D**2)/(4*d* r**3)) #multiplicamos por uma constante 10**17 apenas por conveniência dos valores de y no gráfico
 
     # plotar no gráfico
     plt.figure()
     plt.plot(theta, B)
-    plt.xlabel('θ (rad)')
+    plt.axis([0, 180, 0, 7])
+    plt.title("Gráfico θ x B")
+    plt.xlabel('Ângulo θ Terra-Sol-Vênus')
+    plt.ylabel('Brilho')
+    plt.text(88, 6.4, "*Considerar θ o menor ângulo TSV")
+    plt.show()
+
+# Função que retorna o gráfico r x Brilho
+def graph1():
+    r = np.linspace((D-d), (D+d), 5000)
+    B = ((10**17)*(r**2 + d**2 + 2*d*r - D**2)/(4*d* r**3))
+    plt.figure()
+    plt. plot(r / 10**6, B)
+    plt.axis([(D-d)/ 10**6, (D+d)/ 10**6, 0, 7])
+    plt.title("Gráfico r x B")
+    plt.xlabel('Distância r da Terra a Vênus (em milhões de km)')
     plt.ylabel('Brilho')
     plt.show()
 
+
 # Retorna o brilho em função do ângulo theta
 def btheta(theta):
-    theta = (theta * math.pi) / 180
-    r = math.sqrt(D**2 + d**2 - 2 * D * d * math.cos(theta)) # converte o ang theta em uma distancia r por lei dos cossenos
-    return b(r)
+    if theta % 180 == 0:
+        return 0
+    else:
+        theta = (theta * math.pi) / 180
+        r = math.sqrt(D**2 + d**2 - 2 * D * d * math.cos(theta)) # converte o ang theta em uma distancia r por lei dos cossenos
+        return b(r)
 
 # Função de brilho máximo para auxiliar em contas
 def maxb():
