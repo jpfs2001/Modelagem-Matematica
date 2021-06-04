698d1e51f0436dd676a9e8d2957b46dfc5849919
Potalej <oapotalej@gmail.com>
2021-06-04 13:42:57 -0300

    Pequena atualização pela coerência dos nomes de funções.

--- Projeto-3/matrizes.py
+++ Projeto-3/matrizes.py
@@ -31,7 +31,7 @@ class Matrizes:
                     l.append(matriz[i][j])
             # no caso em que é a_{ixj}, a linha fica vazia, então a gente ignora ela
             if len(l) != 0: M.append(l)
-        return M
+        return (-1)**(sum(ponto))*self.determinanteLU(M)
 
     def transposta(self, matriz):
         # matriz vazia
@@ -52,13 +52,13 @@ class Matrizes:
             l = []
             # calcula os cofatores e suas determinantes
             for j in range(0, len(matriz)):
-                l.append((-1)**(i+j) * self.determinanteLU(self.decomposicao(self.cofator(matriz, [i, j]))[1]))
+                l.append(self.cofator(matriz, [i, j]))
             M.append(l)
         return self.transposta(M)
 
     def inversa(self, matriz):
         # a inversa de A pode ser dada por A^{-1} = adj(A)/det(A), por isso ela não existe se det(A) = 0
-        return self.multiplicacaoNumeroReal(self.adjunta(matriz), 1/self.determinanteLU(self.decomposicao(matriz)[1]))
+        return self.multiplicacaoNumeroReal(self.adjunta(matriz), 1/self.determinanteLU(matriz))
 
     def determinanteLaplace(self, matriz):
         # a determinante usando o método de Laplace utiliza os cofatores
@@ -69,7 +69,7 @@ class Matrizes:
         # se for uma matriz maior que 2x2, aí tem um produto entre certos elementos pela determinante de seus cofatores
         det = 0
         for i in range(0, len(matriz)):
-            det += (-1)**i * matriz[i][0] * self.determinanteLaplace(self.cofator(matriz, [i, 0]))
+            det += self.cofator(matriz, [i, 0])
         return det
 
     def multiplicacao(self, m1, m2):
@@ -102,7 +102,8 @@ class Matrizes:
                     U[linha][c] -= L[linha][coluna]*U[coluna][c]
         return [L, U]
 
-    def determinanteLU(self, U):
+    def determinanteLU(self, matriz):
+        U = self.decomposicao(matriz)[1]
         det = 1
         for i in range(0, len(U)):
             det *= U[i][i]
