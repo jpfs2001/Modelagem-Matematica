3551929815c198f2abdae91db87771cb332c44ee
Potalej <oapotalej@gmail.com>
2021-06-14 21:54:52 -0300

    Nesse arquivo exibir.py é possível inserir uma matriz e visualizar usando matplotlib. Tem um exemplo lá, inclusive, com matriz aleatória.

---
+++ Projeto-4/exibir.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def matrizAleatoria(tamanho):
+    matriz = []
+    for i in range(tamanho):
+        l = []  
+        for j in range(tamanho):
+            l.append(1-np.random.uniform())
+        matriz.append(l)
+    return matriz
+
+def exibir(matriz):
+    # plota usando a escala de cinza
+    plt.imshow(matriz, aspect='auto', cmap="Greys")
+    plt.axis('off')
+    plt.show()
+
+# exemplo de matriz aleatória de tamanho 100x100
+matriz = matrizAleatoria(100)
+exibir(matriz)
