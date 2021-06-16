5c803e53af566170b7c16a6b0d642e0dbd22d831
Potalej <oapotalej@gmail.com>
2021-06-16 10:14:53 -0300

    ideiazinha que podemos usar na 5 se quiserem

---
+++ Projeto-4/ideia/tomografia de imagem.py
@@ -0,0 +1,36 @@
+from PIL import Image
+from numpy import asarray
+import matplotlib.pyplot as plt
+
+# bota a imagem aí
+image = Image.open('787943.jpg')
+
+# converte para array
+data = asarray(image)
+
+matriz = []
+for i in data:
+    m = []
+    for j in i:
+        try: 
+            a = j[3]
+        except:
+            a = 1
+        
+        soma = 0
+        if a != 0:
+            for k in j:
+                soma += k
+            soma -= a
+            soma /= 765
+        m.append(soma)
+    matriz.append(m)
+
+def exibir(matriz):
+    # plota usando a escala de cinza
+    plt.imshow(matriz, aspect='auto', cmap="Greys")
+    plt.axis('off')
+    plt.show()
+
+# exibe a matriz usando a função do exibir.py la
+exibir(matriz)
