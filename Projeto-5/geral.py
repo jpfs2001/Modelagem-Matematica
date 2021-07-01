9640bd8e8d9ab9854ab9c240881eeebaed04c0e8
Potalej <oapotalej@gmail.com>
2021-07-01 19:07:13 -0300

    pequenas alterações

--- Projeto-5/geral.py
+++ Projeto-5/geral.py
@@ -42,6 +42,7 @@ def autovetor(A, y0, k, margem=0):
             x0 = produtoMatriz(A, y[-1])
             # y.append(produtoNumeroReal(x0, 1/normaEuclidiana(x0)))
             y.append(x0)
+            print(abs(abs(produtoEscalar(y[-1], y[-2]))-1))
 
         erro = abs(abs(produtoEscalar(y[-1], y[-2]))-1)
     else:
@@ -49,9 +50,9 @@ def autovetor(A, y0, k, margem=0):
         y = [y0]
         while erro > margem:
             x0 = produtoMatriz(A, y[-1])
-            y.append(produtoNumeroReal(x0, 1/normaEuclidiana(x0)))
-
-            erro = abs(abs(produtoEscalar(y[-1], y[-2]))-1)
+            y.append(x0)
+            print(erro)
+            erro = abs(produtoEscalar(y[-1], y[-2])-1)
 
     return [y, erro]
 
@@ -64,22 +65,16 @@ def produtoEscalar(A, B):
 def autovalor(A, y):
     return produtoEscalar(y, produtoMatriz(A, y))
 
-
 def somaMatrizes(m1, m2):
-    # cria primeiramente uma matriz de diferença
+    # cria primeiramente uma matriz de soma
     soma = [0]*len(m1) 
-    for i in range(0, len(m1)):
+    for i in range(len(m1)):
         # mesma coisa, só que criando uma linha
         soma[i] = [0]*len(m1)
-        for j in range(0, len(m1)):
-            #calcula a diferença entre cada termo, já que há correspondência
+        for j in range(len(m1)):
+            #calcula a soma entre cada termo, já que há correspondência
             soma[i][j] = m1[i][j] + m2[i][j]
     return soma
-
-### Tarefas ###
-# [ ] Encontrar o A
-# [ ] A partir do A, calcular M = (1-m)A+ mS, onde S(n x n) = [[1/n, ...], ..., [1/n, ...]]
-# [ ] A partir do M, achar o x, dado que x(k) = Mx(k-1)
 
 def calculoM(m, A):
     S = []
@@ -89,22 +84,43 @@ def calculoM(m, A):
             linha.append(1/len(A))
         S.append(linha)
     
-    m1 = produtoNumeroReal(A, 1-m)
-    m2 = produtoNumeroReal(S, m)
+    m1 = produtoNumeroReal(A, 1-m) # (1-m)A
+    m2 = produtoNumeroReal(S, m) # mS
     M = somaMatrizes(m1, m2)
     return M
 
+def chuteInicial(tamanho):
+    chute = []
+    for j in range(tamanho):
+        linha = [1/tamanho]
+        chute.append(linha)
+    return chute
     
-A = [[0, 0, 1, .5], [1/3, 0, 0, 0], [1/3, .5, 0, .5], [1/3, .5, 0, 0]]
-y0 = [[.25], [.25], [.25], [.25]]
+### Tarefas ###
+# [X] Encontrar o A
+# [X] A partir do A, calcular M = (1-m)A+ mS, onde S(n x n) = [[1/n, ...], ..., [1/n, ...]]
+# [X] A partir do M, achar o x, dado que x(k) = Mx(k-1)
+
+A = [
+    [0, 0, 1, .5], 
+    [1/3, 0, 0, 0], 
+    [1/3, .5, 0, .5], 
+    [1/3, .5, 0, 0]
+]
+
+y0 = chuteInicial(len(A))
 k = 40
 m = 0.15
 
 M = calculoM(m, A)
+
 print(f'{"*"*40}\n O programa só calcula o dominante \n{"*"*40} \n M: ')
+
 for mi in M:
     print(mi)
+
 [eigenVet, erro] = autovetor(M, y0, k)
 eigenVet = eigenVet[-1]
-eigenVal = autovalor(A, eigenVet)
-print(f'\n autovetor: {eigenVet} \n autovalor: {eigenVal}')
+# eigenVal = autovalor(A, eigenVet)
+# print(f'\n autovetor: {eigenVet} \n autovalor: {eigenVal}')
+print(f'\n autovetor: {eigenVet}')
